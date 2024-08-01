5ba1730cc0b399beae87c38961ee9279665d62ef
easwartechzar <easwar.techzar@gmail.com>
2024-08-01 11:16:45 +0530

    changes

--- task.js
+++ task.js
@@ -1,10 +1,10 @@
 const array = [
-    { name: "kumar", age: "24" },
-    { name: "saran", age: "25" },
-    { name: "Dhinesh", age: "23" },
-    { name: "Arun", age: "26" }
-  ];
-  
-  const findArray = array.filter(item => JSON.stringify(item) === JSON.stringify({ name: "Arun", age: "26" }));
-  
-  console.log(findArray);
+  { name: "kumar", age: "24" },
+  { name: "saran", age: "25" },
+  { name: "saran", age: "25", gender: "male" },
+  { name: "Dhinesh", age: "23" },
+];
+const findObject = { name: "saran", age: "25" }
+const findArray = array.filter(item => { return Object.keys(findObject)?.every(key => item[key] === findObject[key])});
+
+console.log(Object.entries(findObject));
